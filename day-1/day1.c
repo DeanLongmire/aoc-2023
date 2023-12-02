c955348d2ab6a43cf3bf8eac78b1d66125c225aa
Dean Longmire <deanlongmire100@gmail.com>
2023-12-02 04:44:55 -0500

    Remove all regex stuff

--- day-1/day1.c
+++ day-1/day1.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <regex.h>
 
 int processChar(char hexNum) {
     int intNum;
@@ -15,40 +14,11 @@ int processChar(char hexNum) {
     return intNum;
 }
 
-int processLine(int firstNum, int secondNum, char *line, regex_t *regexes, int lineNumber) {
+int processLine(int firstNum, int secondNum, char *line, int lineNumber) {
     int combinedNum;
-    int regexMatch;
-    regoff_t firstNumPosition = 100;
-    regoff_t secondNumPosition = -1;
-    int firstNumMatch = 0, secondNumMatch = 0;
-    size_t numMatches = 18;
-    regmatch_t *matchInfo = malloc(numMatches * sizeof(regmatch_t));
-
-    for(int i = 0; i < 18; i++) {
-        if(regexec(&regexes[i], line, numMatches, matchInfo, 0) == 0) {
-            printf("Line %d had match for regex %d at position %d\n", lineNumber, i, matchInfo->rm_so);
-            if(matchInfo->rm_so < firstNumPosition) {
-                firstNumPosition = matchInfo->rm_so;
-                firstNumMatch = i;
-            }
-
-            if(matchInfo->rm_so > secondNumPosition) {
-                secondNumPosition = matchInfo->rm_so;
-                secondNumMatch = i;
-            }
-        }
-    }
-
-    if(firstNumPosition == secondNumPosition) {
-        printf("Line %d has two nums back to back %d:%d\n", lineNumber, firstNumPosition, secondNumPosition);
-    }
-
-    printf("Taking match %d and %d for line %d\n", firstNumMatch, secondNumMatch, lineNumber);
 
     combinedNum = firstNum * 10;
     combinedNum += secondNum;
-
-    free(matchInfo);
 
     return combinedNum;
 }
@@ -63,89 +33,58 @@ int main(int argc, char **argv) {
     int firstNum, secondNum, lineSum, totalSum = 0;
     int lineCounter = 0;
     int firstNumSet = 0;
-    int offset;
     char *inputBuf;
 
-    regex_t *regexes = malloc(sizeof(regex_t) * 18);
+    file = fopen("input.txt", "r");
 
-    regcomp(&regexes[0], "^[^0-9]*one", 0); //first 'number' is a string
-    regcomp(&regexes[1], "one[^0-9]*$", 0); //last 'number' is a string
-    regcomp(&regexes[2], "^[^0-9]*two", 0);
-    regcomp(&regexes[3], "two[^0-9]*$", 0);
-    regcomp(&regexes[4], "^[^0-9]*three", 0);
-    regcomp(&regexes[5], "three[^0-9]*$", 0);
-    regcomp(&regexes[6], "^[^0-9]*four", 0);
-    regcomp(&regexes[7], "four[^0-9]*$", 0);
-    regcomp(&regexes[8], "^[^0-9]*five", 0);
-    regcomp(&regexes[9], "five[^0-9]*$", 0);
-    regcomp(&regexes[10], "^[^0-9]*six", 0);
-    regcomp(&regexes[11], "six[^0-9]*$", 0);
-    regcomp(&regexes[12], "^[^0-9]*seven", 0);
-    regcomp(&regexes[13], "seven[^0-9]*$", 0);
-    regcomp(&regexes[14], "^[^0-9]*eight", 0);
-    regcomp(&regexes[15], "eight[^0-9]*$", 0);
-    regcomp(&regexes[16], "^[^0-9]*nine", 0);
-    regcomp(&regexes[17], "nine[^0-9]*$", 0);
+    fseek(file, 0, SEEK_END);
+    fileLength = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    printf("File is %d bytes long.\n", fileLength);
 
-    // file = fopen("input.txt", "r");
+    inputBuf = malloc(fileLength);
 
-    // fseek(file, 0, SEEK_END);
-    // fileLength = ftell(file);
-    // fseek(file, 0, SEEK_SET);
-    // printf("File is %d bytes long.\n", fileLength);
+    fread(inputBuf, 1, fileLength, file);
 
-    // inputBuf = malloc(fileLength);
+    int iLine = 0;
+    int j;
 
-    // fread(inputBuf, 1, fileLength, file);
+    for(int i = 0; i < fileLength; i++) {
+        if(inputBuf[i] == 0x0A) { //new line character
+            lineCounter++;
+            char *line = malloc(iLine + 1);
 
-    // int iLine = 0;
-    // int j;
+            for(j = 0; j <= iLine - 1; j++) {
+                line[j] = inputBuf[i - (iLine - j)];
+            }
+            line[j] = '\0';
 
-    // for(int i = 0; i < fileLength; i++) {
-    //     if(inputBuf[i] == 0x0A) { //new line character
-    //         lineCounter++;
-    //         char *line = malloc(iLine + 1);
+            // printf("Line %d: %s\n", lineCounter, line);
 
-    //         for(j = 0; j <= iLine - 1; j++) {
-    //             line[j] = inputBuf[i - (iLine - j)];
-    //         }
-    //         line[j] = '\0';
+            lineSum = processLine(firstNum, secondNum, line, lineCounter);
+            //printf("Line %d: %d\n", lineCounter, lineSum);
 
-    //         // printf("Line %d: %s\n", lineCounter, line);
-
-    //         lineSum = processLine(firstNum, secondNum, line, regexes, lineCounter);
-    //         //printf("Line %d: %d\n", lineCounter, lineSum);
-
-    //         totalSum += lineSum;
-    //         firstNumSet = 0;
-    //         // printf("Line %d: %d characters long\n",lineCounter,iLine);
-    //         iLine = 0;
-    //         free(line);
-    //         continue;
-    //     } else if(isNum(inputBuf[i])) { //is a number
-    //         if(firstNumSet) {
-    //             secondNum = processChar(inputBuf[i]);
-    //         } else {
-    //             firstNum = processChar(inputBuf[i]);
-    //             secondNum = processChar(inputBuf[i]);
-    //             firstNumSet = 1;
-    //         }
-    //     }
-    //     iLine++;
-    // }
-
-    // printf("%d number of lines in file.\n", lineCounter);
-    // printf("Total sum: %d\n", totalSum);
-
-    // fclose(file);
-    // free(inputBuf);
-
-    char *line = strdup("asdftwoonethree223asd");
-
-    processLine(1, 1, line, regexes, 1);
-
-    for(int i = 0; i < 18; i++) {
-        regfree(&regexes[i]);
+            totalSum += lineSum;
+            firstNumSet = 0;
+            // printf("Line %d: %d characters long\n",lineCounter,iLine);
+            iLine = 0;
+            free(line);
+            continue;
+        } else if(isNum(inputBuf[i])) { //is a number
+            if(firstNumSet) {
+                secondNum = processChar(inputBuf[i]);
+            } else {
+                firstNum = processChar(inputBuf[i]);
+                secondNum = processChar(inputBuf[i]);
+                firstNumSet = 1;
+            }
+        }
+        iLine++;
     }
-    free(regexes);
+
+    printf("%d number of lines in file.\n", lineCounter);
+    printf("Total sum: %d\n", totalSum);
+
+    fclose(file);
+    free(inputBuf);
 }
