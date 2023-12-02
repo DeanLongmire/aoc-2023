0271830b1397e7023ff26060407be9b5cd2c93a2
Dean Longmire <deanlongmire100@gmail.com>
2023-12-02 02:10:58 -0500

    add regex for day 1 part 2

--- day-1/day1.c
+++ day-1/day1.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 
 int processChar(char hexNum) {
     int intNum;
-    char *stringNum = malloc(1);
+    char *stringNum = malloc(2);
 
     stringNum[0] = hexNum;
+    stringNum[1] = '\0';
     intNum = atoi(stringNum);
     free(stringNum);
 
@@ -23,28 +25,7 @@ int processLine(int firstNum, int secondNum) {
 }
 
 int isNum(char num) {
-    switch (num) {
-        case 0x31:
-            return 1;
-        case 0x32:
-            return 1;
-        case 0x33:
-            return 1;
-        case 0x34:
-            return 1;
-        case 0x35:
-            return 1;
-        case 0x36:
-            return 1;
-        case 0x37:
-            return 1;
-        case 0x38:
-            return 1;
-        case 0x39:
-            return 1;
-    }
-
-    return 0;
+    return num > '0' && num <= '9';
 }
 
 int main(int argc, char **argv) {
@@ -54,6 +35,27 @@ int main(int argc, char **argv) {
     int lineCounter = 0;
     int firstNumSet = 0;
     char *inputBuf;
+
+    regex_t *regexes = malloc(sizeof(regex_t) * 18);
+
+    regcomp(&regexes[0], "^[^0-9]*one", 0); //first 'number' is a string
+    regcomp(&regexes[1], "one[^0-9]*$", 0); //last 'number' is a string
+    regcomp(&regexes[2], "^[^0-9]*two", 0);
+    regcomp(&regexes[3], "two[^0-9]*$", 0);
+    regcomp(&regexes[4], "^[^0-9]*three", 0);
+    regcomp(&regexes[5], "three[^0-9]*$", 0);
+    regcomp(&regexes[6], "^[^0-9]*four", 0);
+    regcomp(&regexes[7], "four[^0-9]*$", 0);
+    regcomp(&regexes[8], "^[^0-9]*five", 0);
+    regcomp(&regexes[9], "five[^0-9]*$", 0);
+    regcomp(&regexes[10], "^[^0-9]*six", 0);
+    regcomp(&regexes[11], "six[^0-9]*$", 0);
+    regcomp(&regexes[12], "^[^0-9]*seven", 0);
+    regcomp(&regexes[13], "seven[^0-9]*$", 0);
+    regcomp(&regexes[14], "^[^0-9]*eight", 0);
+    regcomp(&regexes[15], "eight[^0-9]*$", 0);
+    regcomp(&regexes[16], "^[^0-9]*nine", 0);
+    regcomp(&regexes[17], "nine[^0-9]*$", 0);
 
     file = fopen("input.txt", "r");
 
@@ -66,15 +68,20 @@ int main(int argc, char **argv) {
 
     fread(inputBuf, 1, fileLength, file);
 
+    int iLine = 0;
+
     for(int i = 0; i < fileLength; i++) {
         if(inputBuf[i] == 0x0A) { //new line character
             lineCounter++;
 
             lineSum = processLine(firstNum, secondNum);
-            printf("Line %d: %d\n", lineCounter, lineSum);
+            //printf("Line %d: %d\n", lineCounter, lineSum);
 
             totalSum += lineSum;
             firstNumSet = 0;
+            printf("Line %d: %d characters long\n",lineCounter,iLine);
+            iLine = 0;
+            continue;
         } else if(isNum(inputBuf[i])) { //is a number
             if(firstNumSet) {
                 secondNum = processChar(inputBuf[i]);
@@ -84,10 +91,16 @@ int main(int argc, char **argv) {
                 firstNumSet = 1;
             }
         }
+        iLine++;
     }
 
     printf("%d number of lines in file.\n", lineCounter);
     printf("Total sum: %d\n", totalSum);
 
     fclose(file);
+    free(inputBuf);
+    for(int i = 0; i < 18; i++) {
+        regfree(&regexes[i]);
+    }
+    free(regexes);
 }
