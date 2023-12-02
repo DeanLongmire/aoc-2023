29751696ecfcb46a0a4df76812aeac3885f658ce
Dean Longmire <deanlongmire100@gmail.com>
2023-12-02 06:23:34 -0500

    Finsihed day 1 part 2

--- day-1/day1.c
+++ day-1/day1.c
@@ -14,8 +14,118 @@ int processChar(char hexNum) {
     return intNum;
 }
 
-int processLine(int firstNum, int secondNum, char *line, int lineNumber) {
+int isNumString(char c, char* line, int startIndex) {
+    switch(c) {
+        case 'o':
+            if(line[startIndex + 1] == 'n') {
+                if(line[startIndex + 2] == 'e') {
+                    return 1;
+                }
+            }
+            return -1;
+        case 't':
+            if(line[startIndex + 1] == 'w') {
+                if(line[startIndex + 2] == 'o') {
+                    return 2;
+                }
+            } else if(line[startIndex + 1] == 'h') {
+                if(line[startIndex + 2] == 'r') {
+                    if(line[startIndex + 3] == 'e') {
+                        if(line[startIndex + 4] == 'e') {
+                            return 3;
+                        }
+                    }
+                }
+            }
+            return -1;
+        case 'f':
+            if(line[startIndex + 1] == 'o') {
+                if(line[startIndex + 2] == 'u') {
+                    if(line[startIndex + 3] == 'r') {
+                        return 4;
+                    }
+                }
+            } else if(line[startIndex + 1] == 'i') {
+                if(line[startIndex + 2] == 'v') {
+                    if(line[startIndex + 3] == 'e') {
+                        return 5;
+                    }
+                }
+            }
+            return -1;
+        case 's':
+            if(line[startIndex + 1] == 'i') {
+                if(line[startIndex + 2] == 'x') {
+                    return 6;
+                }
+            } else if(line[startIndex + 1] == 'e') {
+                if(line[startIndex + 2] == 'v') {
+                    if(line[startIndex + 3] == 'e') {
+                        if(line[startIndex + 4] == 'n') {
+                            return 7;
+                        }
+                    }
+                }
+            }
+            return -1;
+        case 'e':
+            if(line[startIndex + 1] == 'i') {
+                if(line[startIndex + 2] == 'g') {
+                    if(line[startIndex + 3] == 'h') {
+                        if(line[startIndex + 4] == 't') {
+                            return 8;
+                        }
+                    }
+                }
+            }
+            return -1;
+        case 'n':
+            if(line[startIndex + 1] == 'i') {
+                if(line[startIndex + 2] == 'n') {
+                    if(line[startIndex + 3] == 'e') {
+                        return 9;
+                    }
+                }
+            }
+            return -1;
+    }
+}
+
+int findWord(char c, char *line, int startIndex) {
+    if(c != 'o' && c != 't' && c != 'f' && c != 's' && c != 'e' && c != 'n') {
+        return -1;
+    }
+
+    return isNumString(c, line, startIndex);
+    
+    return -1;
+}
+
+int processLine(int firstNum, int secondNum, int firstNumPos, int secondNumPos, char *line, int lineLength, int lineNumber) {
     int combinedNum;
+    int firstStringFound = 0, secondStringFound = 0, firstString = 0, secondString = 0, firstStringPos = 100, secondStringPos = -1;
+    int num = -1;
+
+    for(int i = 0; i < lineLength; i++) {
+        num = findWord(line[i], line, i);
+        if(firstStringFound && num != -1) {
+            secondString = num;
+            secondStringPos = i;
+        } else if(firstStringFound == 0 && num != -1) {
+            firstString = num;
+            secondString = num;
+            firstStringPos = i;
+            secondStringPos = i;
+            firstStringFound = 1;
+        }
+    }
+
+    if(firstStringPos < firstNumPos) { //string comes first
+        firstNum = firstString;
+    }
+    if(secondStringPos > secondNumPos) { //string comes last
+        secondNum = secondString;
+    }
 
     combinedNum = firstNum * 10;
     combinedNum += secondNum;
@@ -30,7 +140,7 @@ int isNum(char num) {
 int main(int argc, char **argv) {
     FILE *file;
     int fileLength;
-    int firstNum, secondNum, lineSum, totalSum = 0;
+    int firstNum, secondNum, firstNumPos = -1, secondNumPos = -1, lineSum, totalSum = 0;
     int lineCounter = 0;
     int firstNumSet = 0;
     char *inputBuf;
@@ -59,10 +169,8 @@ int main(int argc, char **argv) {
             }
             line[j] = '\0';
 
-            // printf("Line %d: %s\n", lineCounter, line);
-
-            lineSum = processLine(firstNum, secondNum, line, lineCounter);
-            //printf("Line %d: %d\n", lineCounter, lineSum);
+            lineSum = processLine(firstNum, secondNum, firstNumPos, secondNumPos, line, iLine, lineCounter);
+            printf("Line %d: %d\n", lineCounter, lineSum);
 
             totalSum += lineSum;
             firstNumSet = 0;
@@ -73,9 +181,12 @@ int main(int argc, char **argv) {
         } else if(isNum(inputBuf[i])) { //is a number
             if(firstNumSet) {
                 secondNum = processChar(inputBuf[i]);
+                secondNumPos = iLine;
             } else {
                 firstNum = processChar(inputBuf[i]);
                 secondNum = processChar(inputBuf[i]);
+                firstNumPos = iLine;
+                secondNumPos = iLine;
                 firstNumSet = 1;
             }
         }
